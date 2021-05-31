2f7562b6bd2e7b927e7156a36ed3346e0bdb21dc
dalyg-octane <dalyg@octanesystems.mx>
2021-05-31 16:45:16 -0700

    React-Redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from "react"
 import { Home } from "./pages/Home"
+import { useSelector,useDispatch } from "react-redux"
 
 // hellocvxcvxvxvxvxcvxcsdasdasdasd
 
 export const App = () => {
+    /* Redux */
+    const { count } = useSelector((state) => state.counter);
+    const dispatch = useDispatch()
+    /* Redux */
     return (
         <div>
             <Home></Home>
